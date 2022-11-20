697b66dc2d34dd1cd81681fc7991341b298bceff
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-11-20 13:36:02 -0300

    Fundamentos & Setup - Setup do TailwindCSS

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,5 @@
+import './styles/global.css';
+
 export function App() {
-  return <h1 style={{ fontFamily: 'Inter' }}>Hello World</h1>
+  return <h1 className="text-violet-400 font-bold text-4xl">Hello World</h1>
 }
---
+++ src/renderer/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      }
+    },
+  },
+  plugins: [],
+}
