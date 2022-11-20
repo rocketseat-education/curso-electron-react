2ee8fcb7ea7609fa03dc00fb5603e73b6684f423
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-11-20 15:55:00 -0300

    Construção da aplicação - Página do documento

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,25 @@
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      Document
-      <Link to="/">Acessar blank</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold text-xs">
+          TABLE OF CONTENTS
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Banco de dados</ToC.Link>
+            <ToC.Link>Autenticação</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center">
+        asdadsdassda
+      </section>
     </main>
   )
 }
