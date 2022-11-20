d8e401c3cc489707c730acedb0b45a8e3eb6e2fd
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-11-20 14:02:10 -0300

    Fundamentos & Setup - Cores e estilos globais

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/global.css'
 
 export function App() {
-  return <h1 className="text-violet-400 font-bold text-4xl">Hello World</h1>
+  return <div className="h-screen w-screen bg-rotion-900 text-rotion-100"></div>
 }
