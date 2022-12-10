c2a3f8dd6fc774d950bfd79183f58d3f99187c67
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-10 18:43:33 -0300

    Storage e comunicação - Remoção do documento

--- src/renderer/src/components/Header/index.tsx
+++ src/renderer/src/components/Header/index.tsx
@@ -2,19 +2,42 @@ import clsx from 'clsx'
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { Code, CaretDoubleRight, TrashSimple } from 'phosphor-react'
 import * as Breadcrumbs from './Breadcrumbs'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Document } from '@shared/types/ipc'
 
 interface HeaderProps {
   isSidebarOpen: boolean
 }
 
 export function Header({ isSidebarOpen }: HeaderProps) {
+  const { id } = useParams<{ id: string }>()
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+
   const isMacOS = process.platform === 'darwin'
+
+  const { mutateAsync: deleteDocument, isLoading: isDeletingDocument } =
+    useMutation(
+      async () => {
+        await window.api.deleteDocument({ id: id! })
+      },
+      {
+        onSuccess: () => {
+          queryClient.setQueryData<Document[]>(['documents'], (documents) => {
+            return documents?.filter((document) => document.id !== id)
+          })
+
+          navigate('/')
+        },
+      },
+    )
 
   return (
     <div
       id="header"
       className={clsx(
-        'border-b border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag',
+        'border-b h-14 border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag',
         {
           'pl-24': !isSidebarOpen && isMacOS,
           'w-screen': !isSidebarOpen,
@@ -31,27 +54,33 @@ export function Header({ isSidebarOpen }: HeaderProps) {
         <CaretDoubleRight className="h-4 w-4" />
       </Collapsible.Trigger>
 
-      <>
-        <Breadcrumbs.Root>
-          <Breadcrumbs.Item>
-            <Code weight="bold" className="h-4 w-4 text-pink-500" />
-            Estrutura técnica
-          </Breadcrumbs.Item>
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.HiddenItems />
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.Item>Back-end</Breadcrumbs.Item>
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.Item isActive>Untitled</Breadcrumbs.Item>
-        </Breadcrumbs.Root>
+      {id && (
+        <>
+          <Breadcrumbs.Root>
+            <Breadcrumbs.Item>
+              <Code weight="bold" className="h-4 w-4 text-pink-500" />
+              Estrutura técnica
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.HiddenItems />
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.Item>Back-end</Breadcrumbs.Item>
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.Item isActive>Untitled</Breadcrumbs.Item>
+          </Breadcrumbs.Root>
 
-        <div className="inline-flex region-no-drag">
-          <button className="inline-flex items-center gap-1 text-rotion-100 text-sm hover:text-rotion-50">
-            <TrashSimple className="h-4 w-4" />
-            Apagar
-          </button>
-        </div>
-      </>
+          <div className="inline-flex region-no-drag">
+            <button
+              disabled={isDeletingDocument}
+              onClick={() => deleteDocument()}
+              className="inline-flex items-center gap-1 text-rotion-100 text-sm hover:text-rotion-50 disabled:opacity-60"
+            >
+              <TrashSimple className="h-4 w-4" />
+              Apagar
+            </button>
+          </div>
+        </>
+      )}
     </div>
   )
 }
