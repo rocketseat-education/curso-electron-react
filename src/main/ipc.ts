5b7a9cd8f5983046b35ba75e99ab0a768cc6bf5b
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-12-10 18:17:36 -0300

    Storage e comunicação - Conectando com o store

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,18 +1,72 @@
 import { ipcMain } from 'electron'
+import { randomUUID } from 'node:crypto'
 
 import { IPC } from '@shared/constants/ipc'
-import { FetchAllDocumentsResponse } from '@shared/types/ipc'
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  Document,
+  FetchAllDocumentsResponse,
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  SaveDocumentRequest,
+} from '@shared/types/ipc'
+
+import { store } from './store'
 
 ipcMain.handle(
   IPC.DOCUMENTS.FETCH_ALL,
   async (): Promise<FetchAllDocumentsResponse> => {
     return {
-      data: [
-        { id: '1', title: 'Ignite', content: '' },
-        { id: '2', title: 'Discover', content: '' },
-        { id: '3', title: 'Rocketseat', content: '' },
-        { id: '4', title: 'Documentação', content: '' },
-      ],
+      data: Object.values(store.get('documents')),
     }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH,
+  async (_, { id }: FetchDocumentRequest): Promise<FetchDocumentResponse> => {
+    const document = store.get(`documents.${id}`) as Document
+
+    return {
+      data: document,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.CREATE,
+  async (): Promise<CreateDocumentResponse> => {
+    const id = randomUUID()
+
+    const document: Document = {
+      id,
+      title: 'Untitled',
+    }
+
+    store.set(`documents.${id}`, document)
+
+    return {
+      data: document,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.SAVE,
+  async (_, { id, title, content }: SaveDocumentRequest): Promise<void> => {
+    store.set(`documents.${id}`, {
+      id,
+      title,
+      content,
+    })
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.DELETE,
+  async (_, { id }: DeleteDocumentRequest): Promise<void> => {
+    // @ts-ignore (https://github.com/sindresorhus/electron-store/issues/196)
+    store.delete(`documents.${id}`)
   },
 )
